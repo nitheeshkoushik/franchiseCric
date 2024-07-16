a5af96ffde8cfa10a8a87bf4fabbea72fe06876f
nitheeshkoushik <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-07-16 12:33:40 -0400

    removed init from loader

--- utils/loader.py
+++ utils/loader.py
@@ -1,19 +1,15 @@
 from google.cloud import bigquery
 
 class Loader:
-    def __init__(self, df, dataset, table):
-        self.df = df 
-        self.dataset = dataset
-        self.table = table
-    
-    def loadData(self):
+
+    def loadData(self,  df ,dataset ,table):
         client = bigquery.Client()
-        dataset_ref = client.dataset(self.dataset)
-        table_ref = dataset_ref.table(self.table)
+        dataset_ref = client.dataset(dataset)
+        table_ref = dataset_ref.table(table)
         job_config = bigquery.LoadJobConfig()
         job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
         job = client.load_table_from_dataframe(
-            self.df, table_ref, job_config=job_config
+            df, table_ref, job_config=job_config
         )
 
         job.result()
