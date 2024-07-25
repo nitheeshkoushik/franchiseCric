1b7d3fac9fdc9da677d0b926cdbc28e785266d85
nitheeshkoushik <nitheeshkoushikgattu@Nitheeshs-MacBook-Air.local>
2024-07-25 13:10:21 -0400

    added Podcasts page and Current Standings page

---
+++ pages/CurrentLeagues.py
@@ -0,0 +1,46 @@
+import streamlit as st 
+from PIL import Image
+from io import BytesIO
+import requests
+from google.cloud import bigquery
+from utils.getDFLoc import GetDFLoc
+
+dfLocator = GetDFLoc()
+client = bigquery.Client()
+
+def readData():
+    currentDFLoc = dfLocator.getDFLocation('current_df')
+    query = f"""
+    SELECT *
+    FROM `{currentDFLoc}`
+    """
+    df = client.query_and_wait(query).to_dataframe()
+    return df
+
+def pageStructure(df):
+    st.set_page_config(layout="wide")
+    st.title("All things Franchise Cricket")
+
+    st.header("Leagues happening this month")
+    filter_options = st.selectbox("Select League", df["league"].unique())
+    filtered_df = df[df["league"] == filter_options].reset_index()
+    return filtered_df
+
+def pointsTable(filtered_df):
+
+    newDF = filtered_df.sort_values(by = ['points', 'nrr'], ascending = False)
+    newDF = newDF[['teamFullName', 'matchesPlayed', 'matchesWon', 'matchesLost', 'points', 'nrr']]
+    newDF = newDF.rename({'teamFullName': 'Team', 
+                            'matchesPlayed': 'Played', 
+                            'matchesWon': 'Won', 
+                            'matchesLost': 'Lost', 
+                            'points': 'Points', 
+                            'nrr': 'Run Rate'}, axis = 1)
+    newDF.index = newDF.index + 1
+
+    st.table(newDF)
+
+if __name__ == "__main__":
+    df = readData()
+    filtered_df = pageStructure(df)
+    pointsTable(filtered_df)
--- pages/Podcasts.py
+++ pages/Podcasts.py
@@ -10,77 +10,74 @@ dfLocator = GetDFLoc()
 client = bigquery.Client()
 
 def readData():
-    newsDFLoc = dfLocator.getDFLocation('news_df')
+    youtubeDFLoc = dfLocator.getDFLocation('youtube_df')
     query = f"""
     SELECT *
-    FROM `{newsDFLoc}`
+    FROM `{youtubeDFLoc}`
     """
     df = client.query_and_wait(query).to_dataframe()
     return df
-
 
 def pageStructure(df):
     st.set_page_config(layout="wide")
     st.title("All things Franchise Cricket")
 
-    st.header("News")
-    filter_options = st.selectbox("Select League", df["League"].unique())
-    filtered_df = df[df["League"] == filter_options]
+    st.header("Podcasts and Videos")
+    filter_options = st.selectbox("Select League", df["league"].unique())
+    filtered_df = df[df["league"] == filter_options]
     return filtered_df
+
 
 def strucureAricles(filtered_df):
     resized_images = []
     titles = []
     urls = []
-    sources = []
+    videos = []
     for index, row in filtered_df.iterrows():
         if len(resized_images) < 5:
             try:
-                image = row["urlToImage"]
+                image = row["url"]
                 response = requests.get(image)
                 image = Image.open(BytesIO(response.content))
                 resized_images.append(image.resize((280, 180)))
                 titles.append(row["title"])
                 urls.append(row["url"])
-                sources.append(row["source_name"])
+                videos.append(f'https://www.youtube.com/watch?v={row["videoID"]}')
             except:
                 continue
-    return resized_images, titles, urls, sources
+    return resized_images, titles, urls, videos
 
-def writeArticles(resized_images, titles, urls, sources):
+def writeArticles(resized_images, titles, urls, videos):
     col1, col2, col3, col4 = st.columns(4)
 
     try:
         with col1:
             st.image(resized_images[0],width=360,use_column_width=True)
             st.subheader(titles[0])
-            st.markdown(f'{sources[0]}')
-            st.markdown(f"[Open article]({urls[0]})")
+            # st.markdown(f'{sources[0]}')
+            st.markdown(f"[Full Video]({videos[0]})")
             
         with col2:
             st.image(resized_images[1],width=360,use_column_width=True)
             st.subheader(titles[1])
-            st.markdown(f'{sources[1]}')
-            st.markdown(f"[Open article]({urls[1]})")
+            # st.markdown(f'{sources[1]}')
+            st.markdown(f"[Full Video]({videos[1]})")
 
 
         with col3:
             st.image(resized_images[2],width=360,use_column_width=True)
             st.subheader(titles[2])
-            st.markdown(f'{sources[2]}')
-            st.markdown(f"[Open article]({urls[2]})")
+            st.markdown(f"[Full Video]({videos[2]})")
 
         with col4:
             st.image(resized_images[3],width=360,use_column_width=True)
             st.subheader(titles[3])
-            st.markdown(f'{sources[3]}')
-            st.markdown(f"[Open article]({urls[3]})")
+            st.markdown(f"[Full Video]({videos[3]})")
     except:
         pass 
-
 
 if __name__ == "__main__":
     df = readData()
     filtered_df = pageStructure(df)
-    resized_images, titles, urls, sources = strucureAricles(filtered_df)
-    writeArticles(resized_images, titles, urls, sources)
+    resized_images, titles, urls, videos = strucureAricles(filtered_df)
+    writeArticles( resized_images, titles, urls, videos)
